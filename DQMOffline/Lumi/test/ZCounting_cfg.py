51893e860a6443a3a964ebb536e2d0ab389b099c
david walter <david.walter2@student.kit.edu>
2022-07-13 11:17:01 +0200

    Update test script

--- DQMOffline/Lumi/test/ZCounting_cfg.py
+++ DQMOffline/Lumi/test/ZCounting_cfg.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
-process = cms.Process('RECODQM', Run2_2018)
+process = cms.Process('RECODQM')
 
 # import of standard configurations
 process.load('Configuration/StandardSequences/Services_cff')
@@ -17,10 +16,11 @@ process.load('Configuration.StandardSequences.EDMtoMEAtRunEnd_cff')
 # load DQM
 process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
+process.load('DQMOffline.Configuration.DQMOffline_cff')
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-process.GlobalTag.globaltag = '101X_dataRun2_Prompt_v9'
-#process.GlobalTag.globaltag = '92X_dataRun2_Prompt_v4'
+process.GlobalTag.globaltag = '123X_dataRun3_HLT_v14' 
+
 
 
 # trigger filter
@@ -31,19 +31,18 @@ process.hltHighLevel.HLTPaths = cms.vstring()
 
 from CondCore.CondDB.CondDB_cfi import *
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 process.source = cms.Source("PoolSource",
-                                fileNames = cms.untracked.vstring(
-#'/store/data/Run2017B/SingleMuon/RECO/PromptReco-v1/000/297/218/00000/14C84999-6457-E711-AAE4-02163E0136E0.root'
-'file:/eos/cms/store/data/Run2018D/SingleMuon/AOD/22Jan2019-v2/110000/104D2C01-12D2-0742-BE61-897755323EDE.root'
-                                )
-                                )
+    fileNames = cms.untracked.vstring(
+        'file:/eos/cms/store/data/Run2018C/SingleMuon/AOD/12Nov2019_UL2018-v3/240001/AFEC3E96-9E21-9A43-89E4-A8F03C2F4EC0.root' 
+    )
+)
 
 process.source.inputCommands = cms.untracked.vstring("keep *",
                                                          "drop *_MEtoEDMConverter_*_*")
 
 process.options = cms.untracked.PSet(
-  wantSummary = cms.untracked.bool(False),
+  wantSummary = cms.untracked.bool(True),
   Rethrow     = cms.untracked.vstring('ProductNotFound'),
   fileMode    = cms.untracked.string('FULLMERGE')
   )
@@ -53,12 +52,17 @@ from DQMOffline.Lumi.ZCounting_cff import zcounting
 
 process.load("DQMOffline.Lumi.ZCounting_cff") 
 
-#process.zcounting.MuonTriggerNames = cms.vstring("HLT_IsoMu27_v*","HLT_IsoMu24_v*")
-#process.zcounting.MuonTriggerObjectNames = cms.vstring("hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07",
-#                                                      "hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07")
+process.zcounting.MuonTriggerNames = cms.vstring("HLT_IsoMu24_v*")
+process.zcounting.MuonTriggerObjectNames = cms.vstring("hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p08")
 
-process.DQMoutput = cms.OutputModule("DQMRootOutputModule",
-                                     fileName = cms.untracked.string("OUT_step1.root"))
+
+#process.DQMoutput = cms.OutputModule("DQMRootOutputModule",
+#                                     fileName = cms.untracked.string("OUT_step1.root"))
+
+#process.out1 = cms.OutputModule("PoolOutputModule",
+#    outputCommands = cms.untracked.vstring('drop *'),
+#    fileName = cms.untracked.string('dqm.root')
+#)
 
 # Path and EndPath definitions
 process.dqmoffline_step = cms.Path(process.zcounting)
@@ -66,11 +70,12 @@ process.dqmsave_step = cms.Path(process.DQMSaver)
 #process.DQMoutput_step = cms.EndPath(process.DQMoutput)
 
 
+
 # Schedule definition
 process.schedule = cms.Schedule(
     process.dqmoffline_step,
-#    process.DQMoutput_step
+#    process.DQMoutput_step,
     process.dqmsave_step
     )
 
-process.dqmSaver.workflow = '/SingleMuon/Run2017B-PromptReco-v1/RECO'
+# process.dqmSaver.workflow = '/SingleMuon/Run2017B-PromptReco-v1/RECO'
